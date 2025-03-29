e46bd600d5ad1a65bbdf20d3e4270628a8ba09c6
giovanni buccigrossi <giovannib776@gmail.com>
2025-03-28 23:19:58 -0400

    Add project files.

---
+++ DiscordRichPresenceTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+
+namespace DiscordRichPresenceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string clientId = "";
+
+            var client = new DiscordRpcClient(clientId);
+
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = "VS IDE",
+                State = "Windows 10",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "beats",               
+                    LargeImageText = "Visual Studio 2022",
+                    SmallImageKey = "beats",          
+                    SmallImageText = "Visual Studio 2022"
+                }
+            });
+
+            Console.WriteLine("Presence set! Press any key to exit.");
+            Console.ReadKey();
+
+            client.Dispose();
+        }
+    }
+}
